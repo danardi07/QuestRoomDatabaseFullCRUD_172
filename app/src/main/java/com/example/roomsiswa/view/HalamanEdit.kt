1f5b957c2652229aa9a9f70c929d7da8202c9dc6
danardi07 <danar.adi.ft22@mail.umy.ac.id>
2025-08-26 21:26:27 +0700

    menambahkan halaman edit siswa dengan form yang terhubung ke ViewModel untuk update data siswa

---
+++ app/src/main/java/com/example/roomsiswa/view/HalamanEdit.kt
@@ -0,0 +1,57 @@
+package com.example.roomsiswa.view
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.roomsiswa.R
+import com.example.roomsiswa.uicontroller.DestinasiNavigasi
+import com.example.roomsiswa.uicontroller.SiswaTopAppBar
+import com.example.roomsiswa.viewmodel.EditViewModel
+import com.example.roomsiswa.viewmodel.PenyediaViewModel
+import kotlinx.coroutines.launch
+
+object DestinasiEditSiswa : DestinasiNavigasi{
+    override val route = "item_edit"
+    override val titleRes = R.string.edit_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EditSiswaScreen(
+    navigateBack: () -> Unit,
+    onNavigateUp: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EditViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+    Scaffold (
+        topBar = {
+            SiswaTopAppBar(
+                title = stringResource(DestinasiEditSiswa.titleRes),
+                canNavigateBack = true,
+                navigateUp = onNavigateUp
+            )
+        },
+        modifier = modifier
+    ) {
+        innerPadding ->
+        val coroutineScope = rememberCoroutineScope()
+        EntrySiswaBody(
+            uiStateSiswa = viewModel.uiStateSiswa,
+            onSiswaValueChange = viewModel::updateUiState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.updateSiswa()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
+}
